4937c296a5dd8ae763f49378e068ac9807be31fc
szystec <toffikers@gmail.com>
2015-02-21 19:12:22 +0000

    fixed 5

--- First Tutorial/js/main.js
+++ First Tutorial/js/main.js
@@ -1,33 +1,32 @@
 var app = {
 
-    findByName: function() {
-      var self = this;  
-      this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-      });
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     },
 
-	showAlert: function (message, title)	{
-		if (navigator.notification){
-				navigator.notification.alert(message, null, title, 'OK');
-		} else {
-			alert(title ? (title + ": " + message) : message);
-		}
-	},
-	
-  renderHomeView: function() {
-    $('body').html(this.homeTpl());
-    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-  },
-  
-  initialize: function() {
-    this.homeTpl = Handlebars.compile($("#home-tpl").html());
-    this.employeeLiTpl = Handlebards.compile($("#employee-li-tpl").html());
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
 
-    var self = this;
-        this.store = new MemoryStore(function () {
-				self.renderHomeView();
-		});
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    initialize: function() {
+        var self = this;
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.store = new MemoryStore(function() {
+            self.renderHomeView();
+        });
     }
 
 };
