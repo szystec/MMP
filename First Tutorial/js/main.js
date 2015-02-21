6da55dc378d80ef673d619a7c3b14b63f6e9df15
szystec <toffikers@gmail.com>
2015-02-21 18:52:25 +0000

    5 tutorial

--- First Tutorial/js/main.js
+++ First Tutorial/js/main.js
@@ -1,16 +1,10 @@
 var app = {
 
     findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
+      var self = this;  
+      this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+      });
     },
 
 	showAlert: function (message, title)	{
@@ -22,18 +16,15 @@ var app = {
 	},
 	
   renderHomeView: function() {
-    var html = 
-        "<div class='header'> <h1>Home</h1> </div>" +
-        "<div class='search-view'>" +
-        "<input class='search-key' />" +
-        "<ul class='employee-list'></ul>" +
-        "</div>"
-    $('body').html(html);
+    $('body').html(this.homeTpl());
     $('.search-key').on('keyup', $.proxy(this.findByName, this));
   },
   
   initialize: function() {
-		var self = this;
+    this.homeTpl = Handlebars.compile($("#home-tpl").html());
+    this.employeeLiTpl = Handlebards.compile($("#employee-li-tpl").html());
+
+    var self = this;
         this.store = new MemoryStore(function () {
 				self.renderHomeView();
 		});
